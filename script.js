1aa78ebc3e73ea03b19a1ff660c9d9c90472f752
Muna Zeki <munazeki@Munas-MacBook-Pro.local>
2023-01-04 21:42:48 -0600

    update

--- script.js
+++ script.js
@@ -15,37 +15,33 @@ function record() {
   var timeKey = (this.parentElement.id)
   var plan = ($(this.parentElement).find(".description")[0].value)
   window.localStorage.setItem(timeKey, plan)
- // eventTime()
+  // eventTime()
 };
 $("#currentDay").text(dayjs().format('dddd, MMM D'))
 function eventTime() {
   var timeBlocks = ($(".time-block"))
-   // Get current hr from day.js
-   timeBlocks.each(function(){
-     var timeblockId= parseInt($(this).attr("id"))
-     if (timeblockId<currentHour){
+  // Get current hr from day.js
+  timeBlocks.each(function () {
+    var timeblockId = parseInt($(this).attr("id"))
+    if (timeblockId < currentHour) {
       $(this).removeClass("future")
       $(this).removeClass("present")
       $(this).addClass("past")
-     }
-     else if (timeblockId===currentHour){
+    }
+    else if (timeblockId === currentHour) {
       $(this).removeClass("future")
       $(this).removeClass("past")
-     $(this).addClass("present")
-    
-     }
-     else {
+      $(this).addClass("present")
+
+    }
+    else {
       $(this).removeClass("present")
       $(this).removeClass("past")
       $(this).addClass("future")
-      
-     }
-  
-   })
-   // Grab the ID and parse to find the hour and determine where in time
-   //console.log(timeblockId)
-   //var currentHour= (Number(timeblockId.slice(-2)))
-  // Add and remove past, present, and future
-    //console.log(currentHour)
-  
+
+    }
+
+  })
+  // Grab the ID and parse to find the hour and determine where in time
+
 }
