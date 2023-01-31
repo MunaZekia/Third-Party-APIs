10a01db4838898c73ec15a293b52c1869092c0e0
Muna Zeki <munazeki@Munas-MacBook-Pro.local>
2023-01-31 16:19:45 -0600

    updated local storage

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ function record() {
   window.localStorage.setItem(timeKey, plan)
   // eventTime()
 };
+$("#09 .description").val(localStorage.getItem("09"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+$("#18 .description").val(localStorage.getItem("18"))
+$("#19 .description").val(localStorage.getItem("19"))
+$("#20 .description").val(localStorage.getItem("20"))
 $("#currentDay").text(dayjs().format('dddd, MMM D'))
 function eventTime() {
   var timeBlocks = ($(".time-block"))
